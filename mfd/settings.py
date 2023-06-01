7bc41eced3d3389b20347a8a18d0d32779944a51
Victor-Agbo <victoragbo72@gmail.com>
2023-06-02 01:47:30 +0530

    Allowed host

--- mfd/settings.py
+++ mfd/settings.py
@@ -32,8 +32,9 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["web-production-ab11.up.railway.app", "127.0.0.1"]
 
+CSRF_TRUSTED_ORIGINS = ["https://web-production-ab11.up.railway.app"]
 
 # Application definition
 
